0425094b219f387306b0da4c1c65bd6ec1339038
BaeKY <ksc2601@gmail.com>
2019-01-30 15:02:16 +0900

    Basic GraphQL schemas & resolvers merging structure

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = process.env.PLAYGROUND_ENDPOINT || "/playground";
+const GRAPHQL_ENDPOINT: string = process.env.GRAPHQL_ENDPOINT || "/graphql";
+
+const appOptions: Options = {
+    port: PORT,
+    playground: PLAYGROUND_ENDPOINT,
+    endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleAppStart = () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+}
+
+app.start(appOptions, handleAppStart)
+    .catch(err => {
+        throw err;
+    });
