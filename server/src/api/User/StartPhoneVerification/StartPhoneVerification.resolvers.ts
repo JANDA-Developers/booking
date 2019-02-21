ad36f396636b71357affe3255d975db07289946f
BaeKY <ksc2601@gmail.com>
2019-02-21 15:18:47 +0900

    PhoneVerification Bug Fix

--- server/src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
+++ server/src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -1,7 +1,8 @@
+import { ObjectId } from "bson";
 import { Target, VerificationModel } from "../../../models/Verification";
 import { StartPhoneVerificationResponse } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
-import privateResolver from "../../../utils/privateResolver";
+import privateResolver from "../../../utils/privateResolvers";
 import { sendVerificationSMS } from "../../../utils/sendSMS";
 
 const resolvers: Resolvers = {
@@ -22,16 +23,23 @@ const resolvers: Resolvers = {
                     });
                     console.log({
                         userId: user._id,
-                        vfId: !existingVerification || existingVerification.user
+                        vfId: existingVerification,
                     });
-                    
-                    if (existingVerification && existingVerification.user.toString() === user._id.toString()) {
+                    if(existingVerification){
+                        console.log({
+                            id1: existingVerification.user,
+                            id2: user._id,
+                            validate: existingVerification.user.equals(user._id)
+                        });
+                    }
+                    if (existingVerification && existingVerification.user.equals(user._id)) {
                         await existingVerification.remove();
                     }
 
                     const verification = new VerificationModel({
                         target: Target.PHONE,
-                        payload: phoneNumber
+                        payload: phoneNumber,
+                        user: new ObjectId(user._id)
                     });
                     await verification.save();
                     sendVerificationSMS(phoneNumber, verification.key);
