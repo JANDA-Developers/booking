fe4d1e41600f189b0389c89323286cd7003ab037
SuperWallaby <colton950901@gmail.com>
2019-03-26 12:34:39 +0900

    1123123asd

---
+++ client/src/components/headers/HeaderWrap.jsx
@@ -0,0 +1,37 @@
+import { Mutation } from 'react-apollo';
+import React from 'react';
+import { toast } from 'react-toastify';
+import { withRouter } from 'react-router-dom';
+import Header from './Header';
+import { LOG_USER_OUT, SELECT_HOUSE } from '../../queries';
+
+const HeaderWrap = ({ history, ...props }) => (
+  <Mutation
+    mutation={LOG_USER_OUT}
+    onCompleted={() => {
+      toast.success('로그아웃 완료');
+      history.replace('./');
+    }}
+  >
+    {logOutMutation => (
+      <Mutation
+        mutation={SELECT_HOUSE}
+        nError={(error) => {
+          console.error('error');
+          console.warn(error);
+        }}
+        onCompleted={({ selectHouse }) => {
+          if (selectHouse && selectHouse.ok) {
+            console.log(selectHouse);
+          }
+        }}
+      >
+        {selectHouseMutation => (
+          <Header {...props} logOutMutation={logOutMutation} selectHouseMutation={selectHouseMutation} />
+        )}
+      </Mutation>
+    )}
+  </Mutation>
+);
+
+export default withRouter(HeaderWrap);
