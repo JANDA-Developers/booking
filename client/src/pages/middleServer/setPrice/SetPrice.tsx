cb5a6bbe52667a01232d21c17f88b25e5d345be0
SuperWallaby <colton950901@gmail.com>
2019-04-11 12:40:45 +0900

    <캐피탈 망할>

---
+++ client/src/pages/middleServer/setPrice/SetPrice.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ErrProtecter from '../../../utils/ErrProtecter';
+import './SetPrice.scss';
+
+interface IProps {}
+
+const SetPrice: React.SFC<IProps> = () => <div />;
+
+export default ErrProtecter(SetPrice);
