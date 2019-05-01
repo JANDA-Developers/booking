10ac96500ca1ff5d89c464f406c3708d11517959
SuperWallaby <colton950901@gmail.com>
2019-05-01 23:46:43 +0900

    뀨

--- client/src/utils/SearchListFormat.tsx
+++ client/src/utils/SearchListFormat.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-restricted-syntax */
 
-const searchListMaker = (dataList, asName, asDetail) => {
-  let formatedArray = [];
+const searchListMaker = (dataList: Array<any>, asName: string, asDetail?: string) => {
+  let formatedArray: { name: string; detail?: string }[] = [];
   if (dataList) {
     formatedArray = dataList.map((value) => {
-      const tempObject = {};
+      const tempObject = { name: 'asName Not Matched', detail: 'asDetail Not Matched' };
       for (const key in value) {
         if (Object.prototype.hasOwnProperty.call(value, key)) {
           const inValue = value[key];
