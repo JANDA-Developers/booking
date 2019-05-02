fcc861c2ab82717eabd964a2ce71cbf75e95afd0
BaeKY <ksc2601@gmail.com>
2019-05-02 14:48:48 +0900

    CreateBooking 버그 수정

--- server/src/api/Booking/CreateBooking/CreateBooking.resolvers.ts
+++ server/src/api/Booking/CreateBooking/CreateBooking.resolvers.ts
@@ -3,6 +3,7 @@ import { InstanceType } from "typegoose";
 import { BookerModel } from "../../../models/Booker";
 import { BookingModel, BookingSchema } from "../../../models/Booking";
 import { GuestModel } from "../../../models/Guest";
+import { HouseModel } from "../../../models/House";
 import { extractBookings } from "../../../models/merge/merge";
 import {
     CreateBookingMutationArgs,
@@ -26,37 +27,59 @@ const resolvers: Resolvers = {
                 await bookerInstance.hashPassword();
                 const bookerId = new Types.ObjectId(bookerInstance._id);
                 // Booker 생성 완료
+                const house = await HouseModel.findById(booker.house);
+                if (!house) {
+                    return {
+                        ok: false,
+                        error: "존재하지 않는 HouseID",
+                        booking: []
+                    };
+                }
+                const houseObjectId = new Types.ObjectId(house._id);
                 const bookings: Array<
                     InstanceType<BookingSchema>
                 > = await Promise.all(
-                    await guest.map(async gst => {
-                        const booking = await new BookingModel({
-                            house: new Types.ObjectId(booker.house),
-                            booker: new Types.ObjectId(bookerId),
-                            roomType: new Types.ObjectId(gst.roomTypeId),
-                            price: gst.price,
-                            discountedPrice: gst.discountedPrice,
-                            start,
-                            end
-                        });
-                        const guests: Types.ObjectId[] = [];
-                        for (let i = 0; i < gst.count; i++) {
-                            guests.push(
-                                (await new GuestModel({
-                                    house: new Types.ObjectId(booker.house),
-                                    booker: bookerId,
-                                    roomType: new Types.ObjectId(
-                                        gst.roomTypeId
-                                    ),
-                                    booking: new Types.ObjectId(booking._id),
-                                    pricingType: gst.pricingType,
-                                    gender: gst.genders && gst.genders[i]
-                                }).save())._id
-                            );
+                    await guest.map(
+                        async ({
+                            price,
+                            discountedPrice,
+                            pricingType,
+                            genders,
+                            roomTypeId,
+                            count
+                        }) => {
+                            const booking = await new BookingModel({
+                                house: houseObjectId,
+                                booker: new Types.ObjectId(bookerId),
+                                roomType: new Types.ObjectId(roomTypeId),
+                                price,
+                                discountedPrice,
+                                start,
+                                end
+                            });
+                            const guests: Types.ObjectId[] = [];
+                            for (let i = 0; i < count; i++) {
+                                guests.push(
+                                    (await new GuestModel({
+                                        house: houseObjectId,
+                                        booker: bookerId,
+                                        roomType: new Types.ObjectId(
+                                            roomTypeId
+                                        ),
+                                        booking: new Types.ObjectId(
+                                            booking._id
+                                        ),
+                                        start,
+                                        end,
+                                        pricingType,
+                                        gender: genders && genders[i]
+                                    }).save())._id
+                                );
+                            }
+                            booking.guests = guests;
+                            return await booking.save();
                         }
-                        booking.guests = guests;
-                        return await booking.save();
-                    })
+                    )
                 );
                 bookerInstance.bookings = bookings.map(
                     b => new Types.ObjectId(b._id.toString())
