a3d9a88ab2523f5260f772b2f6814429a91c44f9
BaeKY <ksc2601@gmail.com>
2019-05-31 16:37:40 +0900

    SendSmsWithTemplate

---
+++ server/src/api/Sms/SendSmsWithTemplate/SendSmsWithTemplate.resolvers.ts
@@ -0,0 +1,81 @@
+import { Types } from "mongoose";
+import { SmsInfoModel } from "../../../models/SmsInfo";
+import {
+    SendSmsWithTemplateMutationArgs,
+    SendSmsWithTemplateResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import { digitsComma } from "../../../utils/etc";
+import { privateResolver } from "../../../utils/privateResolvers";
+import { sendSMS } from "../../../utils/sendSMS";
+import { getFormattedAutoSendMessage } from "../../../utils/SmsDecorator";
+
+const resolvers: Resolvers = {
+    Mutation: {
+        SendSmsWithTemplate: privateResolver(
+            async (
+                _,
+                {
+                    receiver,
+                    smsInfoId,
+                    smsTemplateId,
+                    params
+                }: SendSmsWithTemplateMutationArgs
+            ): Promise<SendSmsWithTemplateResponse> => {
+                try {
+                    const smsInfo = await SmsInfoModel.findOne(
+                        {
+                            _id: new Types.ObjectId(smsInfoId)
+                        },
+                        {
+                            smsTemplates: {
+                                $elemMatch: {
+                                    _id: new Types.ObjectId(smsTemplateId)
+                                }
+                            },
+                            sender: 1
+                        }
+                    );
+                    if (!smsInfo) {
+                        return {
+                            ok: false,
+                            error: "존재하지 않는 smsInfoId"
+                        };
+                    }
+                    const smsTemplate = smsInfo.smsTemplates[0];
+                    if (!smsTemplate) {
+                        return {
+                            ok: false,
+                            error: "존재하지 않는 smsTemplateId"
+                        };
+                    }
+                    const msg = getFormattedAutoSendMessage(
+                        smsTemplate.smsFormat,
+                        {
+                            BOOKERNAME: params.bookerName || "",
+                            ROOMTYPE_N_COUNT: params.RoomTypeNCount || "",
+                            STAY_DATE: params.stayDate || "",
+                            STAY_DATE_YMD: params.stayDateYMD || "",
+                            TOTAL_PRICE: digitsComma(params.totalPrice || 0)
+                        }
+                    );
+                    const sendResult = await sendSMS(
+                        receiver,
+                        msg,
+                        smsInfo.sender.phoneNumber
+                    );
+                    return {
+                        ...sendResult
+                    };
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message
+                    };
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
