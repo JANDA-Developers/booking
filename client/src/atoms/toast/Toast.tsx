247581ed53fa0274c0bbfc8ed3a2d050a701f91c
SuperWallaby <colton950901@gmail.com>
2019-06-13 14:40:21 +0900

    .

--- client/src/atoms/toast/Toast.tsx
+++ client/src/atoms/toast/Toast.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ToastContainer, cssTransition, ToastContainerProps } from 'react-toastify';
-import './Toast.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  ToastContainer,
+  cssTransition,
+  ToastContainerProps
+} from "react-toastify";
+import "./Toast.scss";
 
 //  enter 와 exit 의 값은 css 에니메이션 이름입니다.
 const Zoom = cssTransition({
-  enter: 'zoomIn',
-  exit: 'zoomOut',
-  duration: [400, 400],
+  enter: "zoomIn",
+  exit: "zoomOut",
+  duration: [400, 400]
 });
 
 interface IProps extends ToastContainerProps {}
 
-const JDtoast: React.FC<IProps> = ({ autoClose, position, ...props }) => (
-  <ToastContainer position={position} transition={Zoom} {...props} autoClose={autoClose} />
+const JDtoast: React.FC<IProps> = ({...props}) => (
+  <ToastContainer
+    position={"bottom-right"}
+    transition={Zoom}
+    {...props}
+    autoClose={4000}
+  />
 );
-
-JDtoast.defaultProps = {
-  autoClose: 4000,
-  position: 'bottom-right',
-};
 
 export default JDtoast;
