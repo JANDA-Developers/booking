c1d7bdde246a7a54a2e98b4ea5d30f5e1f10b849
BaeKY <ksc2601@gmail.com>
2019-07-08 12:42:50 +0900

    GetRoomTypeDatePrices 인증 해제

--- server/src/api/RoomPrice/GetRoomTypeDatePrices/GetRoomTypeDatePrices.resolvers.ts
+++ server/src/api/RoomPrice/GetRoomTypeDatePrices/GetRoomTypeDatePrices.resolvers.ts
@@ -6,51 +6,42 @@ import {
     GetRoomTypeDatePricesResponse
 } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
-import { privateResolver } from "../../../utils/privateResolvers";
 
 const resolvers: Resolvers = {
     Query: {
-        GetRoomTypeDatePrices: privateResolver(
-            async (
-                _: any,
-                {
-                    houseId,
-                    start,
-                    end,
-                    roomTypeId
-                }: GetRoomTypeDatePricesQueryArgs
-            ): Promise<GetRoomTypeDatePricesResponse> => {
-                try {
-                    const houseInstance = HouseModel.findById(houseId);
-                    if (!houseInstance) {
-                        return {
-                            ok: false,
-                            error: "존재하지 않는 HouseId",
-                            roomTypeDatePrices: []
-                        };
-                    }
+        GetRoomTypeDatePrices: async (
+            _: any,
+            { houseId, start, end, roomTypeId }: GetRoomTypeDatePricesQueryArgs
+        ): Promise<GetRoomTypeDatePricesResponse> => {
+            try {
+                const houseInstance = HouseModel.findById(houseId);
+                if (!houseInstance) {
                     return {
                         ok: false,
-                        error: null,
-                        roomTypeDatePrices: await getDailyRoomTypePriceQuery({
-                            houseId: new Types.ObjectId(houseId),
-                            start: new Date(start),
-                            end: new Date(end),
-                            roomTypeId:
-                                (roomTypeId &&
-                                    new Types.ObjectId(roomTypeId)) ||
-                                undefined
-                        })
-                    };
-                } catch (error) {
-                    return {
-                        ok: false,
-                        error: error.message,
+                        error: "존재하지 않는 HouseId",
                         roomTypeDatePrices: []
                     };
                 }
+                return {
+                    ok: false,
+                    error: null,
+                    roomTypeDatePrices: await getDailyRoomTypePriceQuery({
+                        houseId: new Types.ObjectId(houseId),
+                        start: new Date(start),
+                        end: new Date(end),
+                        roomTypeId:
+                            (roomTypeId && new Types.ObjectId(roomTypeId)) ||
+                            undefined
+                    })
+                };
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message,
+                    roomTypeDatePrices: []
+                };
             }
-        )
+        }
     }
 };
 export default resolvers;
