1368092bbf2476ea6e932a4a2e81c01a7bdf0096
SuperWallaby <colton950901@gmail.com>
2019-11-21 15:00:07 +0900

    임시-커밋

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable */
+import React from "react";
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/ie11";
 import "babel-polyfill";
 import "whatwg-fetch";
 import dotenv from "dotenv";
-import React from "react";
 import ReactDOM from "react-dom";
 import "./style_config/main.scss";
 import App from "./App";
